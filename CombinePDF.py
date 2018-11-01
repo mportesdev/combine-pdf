1a797adc5c5a93027546de3a72b5abc511267621
myrmica-habilis <michalportes1@gmail.com>
2018-11-01 22:32:10 +0100

    save file dialog, nested layouts

--- CombinePDF.py
+++ CombinePDF.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 from PySide2 import QtWidgets, QtCore, QtGui
-import os.path, functools, json
+import os.path, functools, json, PyPDF2
 
 class MainWindow(QtWidgets.QWidget):
 
@@ -15,13 +15,13 @@ class MainWindow(QtWidgets.QWidget):
             with open("config.json") as file:
                 self.config = json.load(file)
         except:
-            self.config = {"Path": os.curdir}
+            self.config = {"Recent open path": os.curdir, "Recent save path": os.curdir, "Number of items": 5}
         self.filenameLabels = []
+        self.filenames = ["" for i in range(self.config["Number of items"])]
         self.removeButtons = []
         self.pagesEdits = []
         self.pagesLabels = []
         self.messageLabels = []
-        layout = QtWidgets.QGridLayout()
         helpText = """Use comma-separated page numbers and page sequences. Example:
 1, 3-5, 8
 
@@ -33,9 +33,11 @@ Repeating is allowed. Example: 1-3, 2, 1-2 will produce page sequence 1, 2, 3, 2
 "A" means all pages. Example: A, A
 will include the whole document twice"""
 
-        for i in range(5):
+        centralLayout = QtWidgets.QGridLayout()
+        for i in range(self.config["Number of items"]):
             browseButton = QtWidgets.QPushButton("Select file...")
-            browseButton.clicked.connect(functools.partial(self.select_file, i))
+            browseButton.clicked.connect(functools.partial(self.open_file, i))
+            # About using partial to connect slots in a loop:
             # https://stackoverflow.com/questions/40193916/how-come-this-code-only-toggles-blue-python-pyqt
             filenameLabel = QtWidgets.QLabel("[no file]")
             filenameLabel.setEnabled(False)
@@ -60,13 +62,15 @@ will include the whole document twice"""
             self.messageLabels.append(messageLabel)
             line = QtWidgets.QLabel()
             line.setFrameStyle(QtWidgets.QFrame.HLine | QtWidgets.QFrame.Sunken)
-            layout.addWidget(browseButton, 5*i + 5, 0)
-            layout.addWidget(filenameLabel, 5*i + 5, 1, 1, 3)
-            layout.addWidget(removeButton, 5*i + 5, 4)
-            layout.addWidget(pagesLabel, 5*i + 6, 0)
-            layout.addWidget(pagesEdit, 5*i + 6, 1, 1, 2)
-            layout.addWidget(messageLabel, 5*i + 6, 3)
-            layout.addWidget(line, 5*i + 8, 0, 1, 5)
+            centralLayout.addWidget(browseButton, 5*i + 5, 0)
+            centralLayout.addWidget(filenameLabel, 5*i + 5, 1, 1, 3)
+            centralLayout.addWidget(removeButton, 5*i + 5, 4)
+            centralLayout.addWidget(pagesLabel, 5*i + 6, 0)
+            centralLayout.addWidget(pagesEdit, 5*i + 6, 1, 1, 2)
+            centralLayout.addWidget(messageLabel, 5*i + 6, 3)
+            centralLayout.addWidget(line, 5*i + 8, 0, 1, 5)
+        centralLayout.setColumnStretch(1, 3)
+        centralLayout.setColumnStretch(3, 4)
 
         buttonExit = QtWidgets.QPushButton("Exit")
         buttonExit.setIcon(QtGui.QIcon("icons/exit.png"))
@@ -74,21 +78,27 @@ will include the whole document twice"""
         buttonExit.setFixedHeight(50)
         self.buttonCombine = QtWidgets.QPushButton("Combine")
         self.buttonCombine.setIcon(QtGui.QIcon("icons/combine.png"))
+        self.buttonCombine.clicked.connect(self.save_file)
         self.buttonCombine.setFixedHeight(50)
         self.buttonCombine.setEnabled(False)
-        layout.addWidget(buttonExit, 1000, 3)
-        layout.addWidget(self.buttonCombine, 1000, 4)
+        bottomLayout = QtWidgets.QHBoxLayout()
+        bottomLayout.addWidget(buttonExit, 0, 1)
+        bottomLayout.addWidget(self.buttonCombine, 0, 2)
 
-        self.setLayout(layout)
+        masterLayout = QtWidgets.QVBoxLayout()
+        masterLayout.addLayout(centralLayout)
+        masterLayout.addLayout(bottomLayout)
+        self.setLayout(masterLayout)
 
-    def select_file(self, n):
-        filenameTuple = QtWidgets.QFileDialog.getOpenFileName(self, "Select a PDF file", self.config["Path"], "PDF files (*.pdf)")
+    def open_file(self, n):
+        filenameTuple = QtWidgets.QFileDialog.getOpenFileName(self, "Open a PDF file", self.config["Recent open path"], "PDF files (*.pdf)")
         filename = filenameTuple[0]
         if filename:
-            self.config["Path"] = os.path.split(filename)[0]
+            self.config["Recent open path"] = os.path.split(filename)[0]
             self.filenameLabels[n].setEnabled(True)
             self.filenameLabels[n].setText(os.path.basename(filename))
             self.filenameLabels[n].setToolTip(filename)
+            self.filenames[n] = filename
             self.removeButtons[n].setVisible(True)
             self.pagesLabels[n].setVisible(True)
             self.pagesEdits[n].setVisible(True)
@@ -100,16 +110,31 @@ will include the whole document twice"""
         self.filenameLabels[n].setText("[no file]")
         self.filenameLabels[n].setEnabled(False)
         self.filenameLabels[n].setToolTip("")
+        self.filenames[n] = ""
         self.removeButtons[n].setVisible(False)
         self.pagesLabels[n].setVisible(False)
         self.pagesEdits[n].setVisible(False)
         self.messageLabels[n].setVisible(False)
-        for i in range(5):
-            if self.filenameLabels[i].text() != "[no file]":
+        for i in range(self.config["Number of items"]):
+            if self.filenames[i]:
                 break
         else:
             self.buttonCombine.setEnabled(False)
-            
+
+    def save_file(self):
+        outputFilenameTuple = QtWidgets.QFileDialog.getSaveFileName(self, "Save PDF file as...", self.config["Recent save path"], "PDF files (*.pdf)")
+        outputFilename = outputFilenameTuple[0]
+        if outputFilename:
+            self.config["Recent save path"] = os.path.split(outputFilename)[0]
+            merger = PyPDF2.PdfFileMerger()
+            for filename in self.filenames:
+                if filename:
+                    # reader = PyPDF2.PdfFileReader(filename)
+                    # print("Appending {}, number of pages: {}.".format(os.path.basename(filename), reader.numPages))
+                    merger.append(filename)
+            merger.write(outputFilename)
+            merger.close()
+
     def run(self, app):
         self.show()
         app.exec_()
@@ -117,6 +142,7 @@ will include the whole document twice"""
     def __del__(self):
         with open("config.json", "w") as file:
             json.dump(self.config, file, indent=4)
+        # super().__del__()
 
 def main():
     app = QtWidgets.QApplication()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# combine-pdf
+# Combine PDF
+Simple GUI for basic PDF merging capabilities of PyPDF2, written in Python & Qt for Python (PySide2)
+
+### To do:
+* implement page ranges 
+* accommodate for very long filenames
+* prevent saving to one of the input files
