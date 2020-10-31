b5c4126e2015f73e008eed7a4962ec98c88d7676
myrmica-habilis <michalportes1@gmail.com>
2020-10-31 13:00:33 +0100

    Import save_image_as_pdf from external library

--- combinepdf/pdf_utils.py
+++ combinepdf/pdf_utils.py
@@ -1,7 +1,8 @@
+import pictureshow
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from reportlab.lib.pagesizes import A4
-from reportlab.lib.utils import ImageReader
-from reportlab.pdfgen.canvas import Canvas
+
+save_image_as_pdf = pictureshow.picture_to_pdf
 
 
 def get_pdf_num_pages(filename):
@@ -25,31 +26,3 @@ def write_combined_pdf(input_items, output_file):
 
     with open(output_file, 'wb') as f:
         writer.write(f)
-
-
-def save_image_as_pdf(img_file, pdf_file, page_size=A4, margin=0,
-                      stretch_small=False):
-    area_width, area_height = page_size[0] - 2*margin, page_size[1] - 2*margin
-
-    img = ImageReader(img_file)
-    img_width, img_height = img.getSize()
-    image_is_big = img_width > area_width or img_height > area_height
-    image_is_wide = img_width / img_height > area_width / area_height
-
-    # calculate scale factor to fit image to area
-    if image_is_big or stretch_small:
-        scale = (area_width / img_width if image_is_wide
-                 else area_height / img_height)
-    else:
-        scale = 1
-
-    img_new_width = img_width * scale
-    img_new_height = img_height * scale
-
-    # center scaled image to area
-    x = margin + (area_width - img_new_width) / 2
-    y = margin + (area_height - img_new_height) / 2
-
-    pdf_canvas = Canvas(pdf_file, pagesize=page_size)
-    pdf_canvas.drawImage(img, x, y, width=img_new_width, height=img_new_height)
-    pdf_canvas.save()
